d353a93eefc932fcf7e5c809abe2088b49486153
sszkdev <suzuki.coding.19@gmail.com>
2025-11-17 00:18:00 +0900

    Initial Commit

---
+++ ios-kaiyo/ios_kaiyoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_kaiyoApp.swift
+//  ios-kaiyo
+//
+//  Created by shion suzuki on 2025/11/17.
+//
+
+import SwiftUI
+
+@main
+struct ios_kaiyoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
